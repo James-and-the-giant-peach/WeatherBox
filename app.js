9a71d6bfe3445de48655cdaffaea973714460ee1
Nick McKendry <nickmckendry@gmail.com>
2017-06-29 11:44:55 -0600

    commented URL's for API calls

---
+++ app.js
@@ -0,0 +1,3 @@
+// api-url:     https://api.mapbox.com/geocoding/v5/mapbox.places/Arvada.json?access_token=pk.eyJ1IjoidHJhcGZvenoiLCJhIjoiY2o0aW95NmtoMDdnYTMzbDhmNHJ1ejI3dCJ9.SgeYMi-v186-3gho5QUDqQ
+
+// denver weather : https://api.darksky.net/forecast/79fde32973c5a1aa460a2b1529c66e3a/39.8028,-105.0875
